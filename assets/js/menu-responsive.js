579c4a27ada02ff28e9c5395f173e6cc977260b1
David_Barragan <david25barragan@gmail.com>
2024-03-03 10:57:22 -0500

    advance

--- assets/js/menu-responsive.js
+++ assets/js/menu-responsive.js
@@ -1,10 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  const menu = document.getElementById('menu')
-  const navMenu = document.getElementById('.nav--menu')
+  const menu = document.querySelector('#menu')
+  const navMenu = document.querySelector('.nav--menu')
 
   menu.addEventListener('click', function () {
     navMenu.classList.toggle('nav--transalate')
   })
 })
-
